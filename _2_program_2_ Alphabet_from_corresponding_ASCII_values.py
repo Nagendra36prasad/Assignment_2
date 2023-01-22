c58fe706d55320ab68a952f88e3b3d3f74b0d87e
Nagendra36prasad <122007988+Nagendra36prasad@users.noreply.github.com>
2023-01-22 22:16:54 +0530

    Update _2_program_2_ Alphabet_from_corresponding_ASCII_values.py

--- _2_program_2_ Alphabet_from_corresponding_ASCII_values.py
+++ _2_program_2_ Alphabet_from_corresponding_ASCII_values.py
@@ -7,12 +7,7 @@
 #  A-z(uppercase start with 65-90) and a-z (lowercase is start 97-122 )
 # Create the dictionary
 my_dict = {}
-
 for i in range(97, 97 + 26):
-  
-    my_dict[chr(i)] = i  # Map character to ascii value
-
+  my_dict[chr(i)] = i 
 print("  Ascii value of lowercase  is : ")
-
-
 print(my_dict)
