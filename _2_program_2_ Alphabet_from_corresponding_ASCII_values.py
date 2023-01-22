f214cb6c3f4ec82ae9620d856f4aacc5653ba629
Nagendra36prasad <122007988+Nagendra36prasad@users.noreply.github.com>
2023-01-22 22:13:27 +0530

    _2_Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
    Sample Output : {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122

---
+++ _2_program_2_ Alphabet_from_corresponding_ASCII_values.py
@@ -0,0 +1,18 @@
+#Write a Python program to print a dictionary whose keys should be the alphabet from a-z
+#  and the value should be corresponding ASCII values
+
+# ASCII stands for the "American Standard Code for Information Interchange".
+
+# we know that ASCII is a 7-bit character set containing 128 characters .
+#  A-z(uppercase start with 65-90) and a-z (lowercase is start 97-122 )
+# Create the dictionary
+my_dict = {}
+
+for i in range(97, 97 + 26):
+  
+    my_dict[chr(i)] = i  # Map character to ascii value
+
+print("  Ascii value of lowercase  is : ")
+
+
+print(my_dict)
